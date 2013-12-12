0be7b382811417ac7d547c231fc2bd883a055d74
Sean Adler <sadler13@cmc.edu>
2013-12-12 13:36:59 -0800

    first tests

--- Tangram/LevelScene.h
+++ Tangram/LevelScene.h
@@ -13,7 +13,7 @@
 #import "LevelSelectionScene.h"
 #import "TemplateNode.h"
 #import "TemplateEdgeNode.h"
-#import "YMCPhysicsDebugger.h"
+#import <YMCPhysicsDebugger.h>
 
 
 @interface LevelScene : SKScene <SKPhysicsContactDelegate>
