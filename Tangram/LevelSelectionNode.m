8eee0d40988b445de02a875bef1e444f01e80985
Josh Rael <joshuarael@gmail.com>
2013-12-12 21:37:20 -0800

    added level selection tiles

--- Tangram/LevelSelectionNode.m
+++ Tangram/LevelSelectionNode.m
@@ -20,24 +20,19 @@
 
 -(id)initWithLevel:(int)level
 {
-    if (self = [super init])
+    
+    NSArray * levelTiles = @[ @"level-tiles-1.png", @"level-tiles-2.png", @"level-tiles-3.png", @"level-tiles-4.png", @"level-tiles-5.png", @"level-tiles-6.png", @"level-tiles-7.png",@"level-tiles-8.png"];
+
+    if (self = [super initWithImageNamed:levelTiles[level]]) 
     {
         self.level = level;
         
-        // create shape
-        [self setColor:[UIColor colorWithHue:0.315 saturation:0.220 brightness:0.875 alpha:1]];
-        [self setSize:CGSizeMake(150.0, 150.0)];
+//        // create shape
+//        [self setColor:[UIColor colorWithHue:0.315 saturation:0.220 brightness:0.875 alpha:1]];
+//        [self setSize:CGSizeMake(150.0, 150.0)];
+        
     }
     return self;
-}
-
-- (void)shouldMoveToCenter
-{
-    moveToCenter = [SKAction moveTo:CGPointMake(400, 600) duration:.75];
-    scaleToFullSize = [SKAction scaleTo:3.0 duration:.75];
-
-    [self runAction:moveToCenter];
-    [self runAction:scaleToFullSize];
 }
 
 @end
